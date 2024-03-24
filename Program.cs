6edf9962923c3ae3917e582d8109c7a54dbfb48d
Your Name <you@example.com>
2024-03-24 22:15:24 +0300

    added store and item class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using sda_onsite_2_inventory_management.src;
+
+internal class Program {
+    private static void Main(string[] args)
+    {
+        Store store = new("Market");
+        Item chocolate = new("Kinder bueno",430);
+        Item cookies = new("classic",100);
+
+        List<Item> items = store.GetItems();
+
+        store.AddItem(chocolate);
+        store.AddItem(cookies);
+
+        Console.WriteLine(items.Count);
+        Console.WriteLine(chocolate.GetName());
+        Console.WriteLine(cookies.GetName());
+        
+        foreach (Item item in items)
+        {
+            Console.WriteLine($"Name = {item.GetName()} Created_At = {item.GetCreatedAt()} Quantity = {item.GetQuantity()}");
+            
+        }
+
+    }
+ 
+
+
+
+
+
+
+
+}
---
+++ src/Store.cs
@@ -0,0 +1,31 @@
+namespace sda_onsite_2_inventory_management.src
+{
+    public class Store
+    {
+        private readonly string _name;
+        private List<Item> _items;
+    
+
+         public Store(string name)
+         {
+            _name = name;
+            _items = [];
+         }
+
+         public bool AddItem(Item newItem)
+        {
+           _items.Add(newItem);
+            return true;
+        }
+        public bool RemoveItem(Item item)
+        {
+            _items.Remove(item);
+            return true;
+        }
+
+        public List<Item> GetItems()
+        {
+            return _items;
+        }
+ }   
+}
